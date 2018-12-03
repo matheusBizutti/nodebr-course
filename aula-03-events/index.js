b39c862da1edb6d806fd083cbf9138a20929aa02
Matheus Bizutti <matheus.bizutti@totvs.com.br>
2018-12-03 13:50:47 -0200

    aula-03-events - EventEmitter

---
+++ aula-03-events/index.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor();
+const nomeEvento = 'usuario:click';
+
+meuEmissor.on(nomeEvento, (click) => {
+  console.log('Um usuário clicou: ' , click);
+});
+
+// meuEmissor.emit(nomeEvento, 'na barra de rolagem');
+// meuEmissor.emit(nomeEvento, 'no ok');
+
+// let count = 0;
+
+// setInterval(() => {
+//   meuEmissor.emit(nomeEvento, 'no ok' + (count++));
+// }, 1000);
+
+const stdin = process.openStdin();
+
+stdin.addListener('data', (value) => {
+  console.log(`You tapped: ${value.toString().trim()}`);
+});
